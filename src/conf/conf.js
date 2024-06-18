795a3bea857acc25249df88c311bffc5aa366ca3
Abhishek6546 <abhishekk11603@gmail.com>
2024-06-18 12:38:05 +0530

    b

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,6 @@ const conf ={
     appwritedatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    tinymceApiKey:String(import.meta.env.VITE_TINYMCE_API_KEY),
 
 }
 console.log("TinyMCE conf.js:", conf.tinymceApiKey); 
